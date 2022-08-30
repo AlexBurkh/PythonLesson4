8b46cd7a7dec4abf1ac3781a1ee83253feaffae5
AlexBurkh <alex.burkh@yandex.ru>
2022-08-30 12:18:38 +0300

    Без 4

--- 4.py
+++
@@ -1,7 +0,0 @@
-def parse_coefficients(equation_string):
-    left = equation_string.split(' = ')
-    members = left.split(' + ').split(' - ')
-    coefficients = []
-    for item in members:
-        coefficients.append(item.split(' * '))
-    return coefficients
